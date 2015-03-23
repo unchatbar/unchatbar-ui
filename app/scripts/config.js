c0a11d43aa3c52099bdcfea1f413d4d55c4d2239
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2015-03-23 19:53:56 +0100

    Profile view

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -19,8 +19,44 @@ angular.module('unchatbar')
           templateUrl: '../views/login.html'
         })
         .state('dashboard', {
-          url: '/dashboard',
+          abstract: true,
           templateUrl: '../views/dashboard.html'
+        })
+        .state('chat', {
+          parent: 'dashboard',
+          url: '/chat',
+          views: {
+            header: {
+              templateUrl: 'views/dashboard/header.html'
+            },
+            sidebar: {
+              templateUrl: 'views/dashboard/sidebar.html'
+            },
+            content: {
+              templateUrl: 'views/dashboard/chat.html'
+            },
+            footer: {
+              templateUrl: 'views/dashboard/footer.html'
+            }
+          }
+        })
+        .state('profile', {
+          parent: 'dashboard',
+          url: '/profile',
+          views: {
+            header: {
+              templateUrl: 'views/dashboard/header.html'
+            },
+            sidebar: {
+              templateUrl: 'views/dashboard/sidebar.html'
+            },
+            content: {
+              templateUrl: 'views/dashboard/profile.html'
+            },
+            footer: {
+              templateUrl: 'views/dashboard/footer.html'
+            }
+          }
         });
 
       $urlRouterProvider.otherwise('/');
